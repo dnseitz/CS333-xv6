7f4c192df68b8449eb3b1518bbf86fc6e10c31ef
Daniel Seitz <dnseitz@gmail.com>
2016-04-09 13:05:29 -0700

    updated syscall.c to print syscalls

--- xv6-psu-kernel/syscall.c
+++ xv6-psu-kernel/syscall.c
@@ -125,6 +125,31 @@ static int (*syscalls[])(void) = {
 [SYS_halt]    sys_halt,
 };
 
+static char* syscallnames[] = {
+[SYS_fork]    "fork",
+[SYS_exit]    "exit",
+[SYS_wait]    "wait",
+[SYS_pipe]    "pipe",
+[SYS_read]    "read",
+[SYS_kill]    "kill",
+[SYS_exec]    "exec",
+[SYS_fstat]   "fstat",
+[SYS_chdir]   "chdir",
+[SYS_dup]     "dup",
+[SYS_getpid]  "getpid",
+[SYS_sbrk]    "sbrk",
+[SYS_sleep]   "sleep",
+[SYS_uptime]  "uptime",
+[SYS_open]    "open",
+[SYS_write]   "write",
+[SYS_mknod]   "mknod",
+[SYS_unlink]  "unlink",
+[SYS_link]    "link",
+[SYS_mkdir]   "mkdir",
+[SYS_close]   "close",
+[SYS_halt]    "halt",
+};
+
 void
 syscall(void)
 {
@@ -133,6 +158,7 @@ syscall(void)
   num = proc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     proc->tf->eax = syscalls[num]();
+    cprintf("%s -> %d\n", syscallnames[num], proc->tf->eax);
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             proc->pid, proc->name, num);
